6d6c15ce13d5a969e96c166ec20502cb75b19c6e
peterjasc <peeter.kokk@gmail.com>
2018-04-07 12:20:27 +0300

    store contract info in logs not storage - cheaper II

--- src/main/java/smartcontract/contracts/SmartContract.sol
+++ src/main/java/smartcontract/contracts/SmartContract.sol
@@ -1,53 +1,18 @@
 pragma solidity ^0.4.20;
+
 contract SmartContract {
-    Contract[] public contracts;
-    event BidAccepted(uint indexed contractId, uint indexed contractIndex);
+    event BidAccepted(uint indexed contractId, uint indexed contractIndex, uint quantity, uint targetPrice, uint targetTime);
+
     uint noOfContracts;
 
-    struct Contract {
-        uint contractId;
-        uint quantity;
-        uint targetPrice;
-        uint targetTime;
-    }
-
-    function SmartContract() {
+    function SmartContract() public {
         noOfContracts = 0;
     }
 
-    function addContract(uint _cId , uint _quantity, uint _targetPrice, uint _targetTime) returns (bool success) {
-        Contract memory newContract; //creates new struct and memory
-        /*Date memory _date;*/
-        newContract.contractId = _cId;
-        newContract.quantity = _quantity;
-        newContract.targetPrice = _targetPrice;
-        newContract.targetTime = _targetTime;
-        contracts.push(newContract);//add elem to array
-        BidAccepted(_cId, contractIndex);
+    function addContract(uint _cId, uint _quantity, uint _targetPrice, uint _targetTime) public returns (bool success) {
+        noOfContracts = noOfContracts + 1;
+        emit BidAccepted(_cId, noOfContracts, _quantity, _targetPrice, _targetTime);
         return true;
-    }
-
-    function getClosedContracts() constant returns (uint[], uint[], uint[], uint[]) {
-        uint length = contracts.length;
-        uint[] memory contractId = new uint[](length);
-        uint[] memory qty = new uint[](length);
-        uint[] memory targetPrice = new uint[](length);
-        uint[] memory targetTime = new uint[](length);
-
-        for (uint i = 0; i < contracts.length; i++) {
-            Contract memory currentContract;
-            currentContract = contracts[i];
-
-              contractId[i] = currentContract.contractId;
-              qty[i] = currentContract.quantity;
-              targetPrice[i] = currentContract.targetPrice;
-              targetTime[i] = currentContract.targetTime;
-        }
-        return (contractId, qty, targetPrice, targetTime);
-    }
-
-    function getContractByIndex(uint _index) constant returns (uint[], uint[], uint[], uint[]) {
-        
     }
 
 }
